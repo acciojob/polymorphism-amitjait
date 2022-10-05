c8730d54f9bf35514648817063bc027d3fc468a6
amitjait <amit.rimit591@gmail.com>
2022-10-05 11:14:19 +0530

    Updating

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,5 +2,25 @@ package com.driver;
 
 public class Main {
 
+        public static void main(String[] args) {
+          Product P = new Product();
+          P.product(1, 2);
+          P.product(2, 3, 4);
+          P.product(1.5, 2.5);
+
+        }
     
+  }
+  class Product{
+    public int product(int x, int y) {
+        return x*y;
+    }
+
+    public int product(int x, int y, int z) {
+      return x*y*z;
+    }
+
+    public double product(double x, double y) {
+      return x*y;
+    }
   }
